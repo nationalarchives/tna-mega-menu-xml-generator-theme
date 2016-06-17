a8df3283fcb9a5a84dc974e8d82171a9dffa5ed2
Punal Chotrani <punalsc@gmail.com>
2016-06-17 14:09:48 +0100

    Housekeeping and commenting.

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-header('Content-Type: text/xml; charset=' . get_option('blog_charset'), true);
+header('Content-Type: text/xml; charset=' . get_option('blog_charset'), true); //This sets the content type to a xml document and renders the proper format in a browser.
 
 $link_cat = '';
 if ( !empty($_GET['link_cat']) ) {
@@ -15,7 +15,7 @@ echo '<?xml version="1.0"?'.">\n";
 $xmlBody = '<opml version="1.0">';
 $xmlBody .= '<head>';
 $xmlBody .= '<title>'.get_the_title().'</title>';
-$xmlBody .= '<dateCreated>'.gmdate("D, d M Y H:i:s").'</dateCreated>';
+$xmlBody .= '<dateCreated>'.gmdate( 'D, d M Y H:i:s' ) . ' GMT</dateCreated>';
 $xmlBody .= '</head>';
 $xmlBody .= '<body>';
 
@@ -25,18 +25,18 @@ if ( empty($link_cat) )
 else
 	$cats = get_categories(array('taxonomy' => 'link_category', 'hierarchical' => 0, 'include' => $link_cat));
 
-foreach ( (array)$cats as $cat ) : //main foreach
+foreach ( (array)$cats as $cat ) : //Loops the Links Categories
 	$catname = apply_filters( 'link_category', $cat->name );
 	$caturl = apply_filters( 'link_category', $cat->slug );
 	$xmlBody .= '<category type="category" title="'.$catname.'" catUrl="'.$caturl.'">';
 
 	$bookmarks = get_bookmarks(array("category" => $cat->term_id, "orderby"  => 'rating'));
-	foreach ( (array)$bookmarks as $bookmark ) : //looping the $bookmarks
+	foreach ( (array)$bookmarks as $bookmark ) : //Loops links of each category. Orders
 		$title = apply_filters( 'link_title', $bookmark->link_name );
 		$xmlBody .= '<item text="'.esc_attr($title).'" type="link" htmlUrl="'.esc_attr($bookmark->link_url).'" linkDesc="'.esc_attr($bookmark->link_description).'"/>';
-	endforeach;//END $bookmarks
+	endforeach;//END loop links
 	$xmlBody .= '</category>';
-endforeach;//END main foreach
+endforeach;//END loop category links
 ?>
 
 <?php
@@ -49,8 +49,8 @@ echo $xmlBody;
 
 <?php
 
-delete_site_transient( 'cache_xml' );
+delete_site_transient( 'cache_xml' ); // Deletes the xml cache on the network, when the new xml if rendered.
 
-$xmlfile = new SimpleXMLElement($xmlBody);
-$xmlfile->asXML('mega_menu_data.xml');
+$xmlfile = new SimpleXMLElement($xmlBody); //Creates a .xml file to the root folder
+$xmlfile->asXML('mega_menu_data.xml'); // Saves it to the root folder under the file name 'mega_menu_data.xml'
 ?>
